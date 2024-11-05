c7837badababa5c53301e8204a2b6d570c9689fb
Dissoares <diegosoaresyt@gmail.com>
2024-11-05 19:09:19 -0300

    adicionando modulo e importando componentes

---
+++ src/app/modulos/gerador-de-pessoas/fomulario-gerador-de-pessoas/fomulario-gerador-de-pessoas.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FomularioGeradorDePessoasComponent } from './fomulario-gerador-de-pessoas.component';
+import { DadosPessoaisDialogComponent } from '../dialogs/pessoa-dados-pessoais/dados-pessoais-dialog/dados-pessoais-dialog.component';
+import { DadosEnderecoDialogComponent } from '../dialogs/pessoa-endereco/dados-endereco-dialog/dados-endereco-dialog.component';
+import { DadosProfissionaisDialogComponent } from '../dialogs/pessoa-dados-profissionais/dados-profissionais-dialog/dados-profissionais-dialog.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    FomularioGeradorDePessoasComponent,
+    DadosPessoaisDialogComponent,
+    DadosEnderecoDialogComponent,
+    DadosProfissionaisDialogComponent
+  ]
+})
+export class FomularioGeradorDePessoasModule { }
