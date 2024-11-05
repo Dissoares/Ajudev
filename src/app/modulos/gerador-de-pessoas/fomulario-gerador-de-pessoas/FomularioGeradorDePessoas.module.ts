b0cee3decb933795e0afc04771cd469bbaeb1f95
Dissoares <diegosoaresyt@gmail.com>
2024-11-05 19:09:52 -0300

    corrigindo import

---
+++ src/app/modulos/gerador-de-pessoas/fomulario-gerador-de-pessoas/FomularioGeradorDePessoas.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FomularioGeradorDePessoasComponent } from './fomulario-gerador-de-pessoas.component'; 
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [FomularioGeradorDePessoasComponent]
+})
+export class FomularioGeradorDePessoasModule { }
