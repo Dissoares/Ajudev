a8601ddbf6cc941a2908d6f76ec6ec30eadbb89a
Dissoares <diegosoaresyt@gmail.com>
2024-11-05 18:46:39 -0300

    adicionando dados de endereço

---
+++ src/app/modulos/gerador-de-pessoas/dialogs/pessoa-endereco/pessoaEndereco/pessoaEndereco.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoaEndereco',
+  templateUrl: './pessoaEndereco.component.html',
+  styleUrls: ['./pessoaEndereco.component.scss']
+})
+export class PessoaEnderecoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
