f665f49ab7095866551edc9e653fd8a99b77f418
Dissoares <diegosoaresyt@gmail.com>
2024-11-05 18:45:57 -0300

    adicionando formulário de dados pessoais

---
+++ src/app/modulos/gerador-de-pessoas/dialogs/pessoa-dados-pessoais/pessoaDadosPessoais/pessoaDadosPessoais.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoaDadosPessoais',
+  templateUrl: './pessoaDadosPessoais.component.html',
+  styleUrls: ['./pessoaDadosPessoais.component.scss']
+})
+export class PessoaDadosPessoaisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
