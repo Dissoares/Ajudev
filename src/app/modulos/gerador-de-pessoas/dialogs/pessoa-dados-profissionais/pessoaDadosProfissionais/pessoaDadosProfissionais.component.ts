1cfefae09f3df2716fcdbe9974ddca5376c28891
Dissoares <diegosoaresyt@gmail.com>
2024-11-05 18:46:15 -0300

    adicionando formulario de dados profissionais

---
+++ src/app/modulos/gerador-de-pessoas/dialogs/pessoa-dados-profissionais/pessoaDadosProfissionais/pessoaDadosProfissionais.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoaDadosProfissionais',
+  templateUrl: './pessoaDadosProfissionais.component.html',
+  styleUrls: ['./pessoaDadosProfissionais.component.scss']
+})
+export class PessoaDadosProfissionaisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
